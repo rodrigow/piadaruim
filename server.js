f67ff49c932959ae587ff3c01e209e718b3e836d
Rodrigo Wolschick <rodrigowolschick@gmail.com>
2019-04-27 11:49:27 -0300

    feat: helmet and joke object as resource

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 
 const express = require('express')
 const path = require('path')
+const helmet = require('helmet')
 
-const app = express()
+const server = express()
 const port = 15751
 
-app.use('/', express.static(path.join(__dirname, 'frontend/build')))
+server.use(helmet())
+server.use('/', express.static(path.join(__dirname, 'frontend/build')))
 
-app.get('/hello', (req, res) => res.send('Hello World!'))
+server.get('/resources/joke', function (req, res) {
+    res.json({"id": 1, "joke": "Como um átomo atende o telefone? PRÓTON?!"})
+})
 
-app.listen(port, () => console.log(`Piada Ruim do Dia running on port ${port}!`))
+server.get('/hello', (req, res) => res.send('Hello World!'))
+
+server.listen(port, () => console.log(`Piada Ruim do Dia running on port ${port}!`))
