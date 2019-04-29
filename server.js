4a9691065f4f8c89fee8b3968b82070e9973a0e6
Rodrigo Wolschick <rodrigowolschick@gmail.com>
2019-04-29 18:16:08 -0300

    feat: procura por id

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require('express')
 const path = require('path')
 const helmet = require('helmet')
@@ -13,20 +12,18 @@ const port = 15751
 server.use(helmet())
 server.use('/', express.static(path.join(__dirname, 'frontend/build')))
 
-server.get('/resources/joke', async function (req, res, next) {
+server.get('/resources/joke/:id?', async function (req, res, next) {
     try {
         const db = await dbPromise
-        const [ joke ] = await Promise.all([db.get('SELECT * FROM Jokes ORDER BY RANDOM() LIMIT 1;')])
-        res.json(joke)
+        const [ joke ] = await Promise.all([db.get(sql(req.params.id))])
+        joke ? res.json(joke) : res.status(404).send({id: req.params.id, text: 'Not Found'})
     } catch (err) {
         next(err)
     }
 })
 
-server.post('', function(req, res) {
-
-})
-
-server.get('/hello', (req, res) => res.send('Hello World!'))
+const sql = id => {
+    return isNaN(id) ? 'SELECT * FROM Jokes ORDER BY RANDOM() LIMIT 1;' : `SELECT * FROM Jokes WHERE id = ${id}`
+}
 
 server.listen(port, () => console.log(`Piada Ruim do Dia running on port ${port}!`))
