188d94f3c18903918e96fdff223534239c68780f
lucasborges <Lborges@gap.com>
2019-05-18 16:40:09 -0300

    fixing some minor issues

--- frontend/src/services/BadJokeService.Dev.js
+++ frontend/src/services/BadJokeService.Dev.js
@@ -6,7 +6,6 @@ export const BadJokeServiceDev = async () => {
             id: 1,
             text: "Por quê as araucárias não se perdem na mata? Pois elas tem uma pinha.",
             totalOfJokes: 1
-        },
-
+        }
     }
 }
