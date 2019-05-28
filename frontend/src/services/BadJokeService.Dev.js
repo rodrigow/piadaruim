f9c87a473a915cb96eb234ba19e7e4dd3e92d532
Rodrigo Wolschick <rodrigowolschick@gmail.com>
2019-05-28 15:45:17 -0300

    feat: creates permalink for jokes

--- frontend/src/services/BadJokeService.Dev.js
+++ frontend/src/services/BadJokeService.Dev.js
@@ -1,9 +1,10 @@
-export const BadJokeServiceDev = async () => {
+export const BadJokeServiceDev = async (id) => {
+    console.log('DEBUG => Bad Joke Service: ' + id)
     return {
         ok: true,
         status: 200,
         joke: {
-            id: 1,
+            id: id ? id : 1,
             text: "Por quê as araucárias não se perdem na mata? Pois elas tem uma pinha."
         }
     }
