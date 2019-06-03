73cfd59c8218c36524debb933dba843a5debc28a
Lucas Borges <lucascomputacao@gmail.com>
2019-06-03 11:15:35 -0300

    Hiding answers and Avoiding repeated jokes (#18)
    * feat: new package verion
    
    * Updating package
    
    * Hiding answer w/ Regex, add button to show answer and fixing migration
    
    * Work around mechanism to avoid repeated jokes
    
    * Modifying way to get a new joke using alreadyReadArray to improve search
    in database
    
    * removing console.log from server.js
    
    * fixing bug in permalink
    
    * Updating to arrow functions

--- frontend/src/BadJoke.js
+++ frontend/src/BadJoke.js
@@ -1,61 +1,92 @@
 import React, { Component } from 'react'
 import './BadJoke.css'
-import { BadJokeService }  from './services/BadJokeService'
+import { BadJokeService}  from './services/BadJokeService'
 
 class BadJoke extends Component {
 
   state = {
-    joke: {id: 0, text: ''}
-  }
+    joke: {id: 0, text: '', answer: ''}
+  };
 
-  callService(id) {
-    BadJokeService(id).then(result => {
+  splitedAnswer = '';
+  idsAlreadyBeenRead = [];
+
+  callService = id => {
+    BadJoke.buttonAnswerVisibility("none");
+    var param = !isNaN(id) ? id : JSON.stringify(this.idsAlreadyBeenRead);
+    BadJokeService(param).then(result => {
         if (result.ok) {
-          this.setState({joke: result.joke})
+            this.splitQuestionAnswerByQuestionMark(result.joke)
         } else {
           this.setState({joke: {id, text: 'Essa piada ainda é tão ruim que ainda não existe!!!' }})
+            this.idsAlreadyBeenRead = [];
         }
       }).catch(e => {
-          console.log(e)
+          console.log(e);
           this.setState({joke: {id: 0, text: "Erro, tente mais tarde"}})
       })
-  }
+  };
 
-  componentDidMount() {
-    const { match: { params } } = this.props
+  static buttonAnswerVisibility = param => {
+    const buttonAnswer = document.getElementById("button-answer");
+    buttonAnswer.style.display = param;
+  };
+
+  componentDidMount = () => {
+    const { match: { params } } = this.props;
     this.callService(params.id)
-  }
+  };
 
-  copyToClipboard() {
-    document.getElementById("permalink").select()
+
+  static copyToClipboard = () => {
+    document.getElementById("permalink").select();
     document.execCommand("copy")
-  }
+  };
 
-  render() {
-    return (
-        <div className="App">
+  getJokesAnswer = joke => {
+    this.setState({joke: {id: joke.id, text: joke.text, answer: 'R: ' + this.splitedAnswer}});
+    BadJoke.buttonAnswerVisibility("none")
+  };
+
+  splitQuestionAnswerByQuestionMark = joke => {
+     const splitJoke = joke.text.match(/\S[^?]*(?:\?+|$)/g);
+     if (splitJoke.length > 1){
+         BadJoke.buttonAnswerVisibility("inline"); // show answer's button
+        this.splitedAnswer = splitJoke[1]; // saving splitedAnswer to be show later
+     }
+     this.setState({joke: {id: joke.id, text: splitJoke[0], answer: ''}})
+     this.idsAlreadyBeenRead.push(joke.id)
+  };
+
+  render = () => (
+      <div className="App">
           <header className="App-header">
-            <p>
-              Piada Ruim do Dia!
-            </p>
+              <p>
+                  Piada Ruim do Dia!
+              </p>
           </header>
           <div className="joke">
-            <p id="badjoke">{this.state.joke.text}</p>
-            <br />
-            <div>
-                <label htmlFor='permalink'>Permalink:</label>
-                <input id='permalink' readOnly type='text' value={`${window.location.origin}/${this.state.joke.id}`}></input>
-                <button className="button-permalink" onClick={this.copyToClipboard}>Copiar</button>
-            </div>
-            <br />
-            <br />
-            <div>
-              <button className="button"  onClick={(e) => this.callService()}>Quero ver uma nova piada!</button>
-            </div>
+              <p id="badjoke">{this.state.joke.text}</p>
+              <p id="splitedAnswer">{this.state.joke.answer}</p>
+              <button id="button-answer" className="button-answer"
+                      onClick={(e) => this.getJokesAnswer(this.state.joke)}>
+                  Ver resposta
+              </button>
+              <br/>
+              <div>
+                  <label htmlFor='permalink'>Permalink:</label>
+                  <input id='permalink' readOnly type='text' className="input"
+                         value={`${window.location.origin}/${this.state.joke.id}`}/>
+                  <button className="button-permalink" onClick={BadJoke.copyToClipboard}>Copiar</button>
+              </div>
+              <br/>
+              <br/>
+              <div>
+                  <button className="button" onClick={(e) => this.callService()}>Quero ver uma nova piada!</button>
+              </div>
           </div>
-        </div>
-    )
-  }
+      </div>
+  );
 }
 
 export default BadJoke
--- server.js
+++ server.js
@@ -11,9 +11,10 @@ const dbPromise = sqlite.open('./piadaruim.sqlite', { Promise })
 const server = express()
 const port = 15751
 
-const sql = id => {
-    return isNaN(id) ? 'SELECT * FROM Jokes ORDER BY RANDOM() LIMIT 1;' : `SELECT * FROM Jokes WHERE id = ${id}`
-}
+const sql = id =>
+    Array.isArray(id) ? `SELECT * FROM Jokes WHERE id NOT IN (${id}) ORDER BY RANDOM() LIMIT 1`
+        : isNaN(id)   ? 'SELECT * FROM Jokes ORDER BY RANDOM() LIMIT 1;'
+        : `SELECT * FROM Jokes WHERE id = ${id}`
 
 server.use(helmet())
 server.use('/', express.static(path.join(__dirname, 'frontend/build')))
@@ -22,7 +23,7 @@ server.use('/static', express.static(path.join(__dirname, 'frontend/build/static
 server.get('/resources/joke/:id?', async function (req, res, next) {
     try {
         const db = await dbPromise
-        const [ joke ] = await Promise.all([db.get(sql(req.params.id))])
+        const [ joke ] = await Promise.all([db.get(sql(JSON.parse(req.params.id)))])
         joke ? res.json(joke) : res.status(404).send({id: req.params.id, text: 'Not Found'})
     } catch (err) {
         next(err)
@@ -32,5 +33,7 @@ server.get('/resources/joke/:id?', async function (req, res, next) {
 server.get('*', (req,res) =>{
     res.sendFile(path.join(__dirname, 'frontend/build/'));
 });
+
+
 
 server.listen(port, () => console.log(`Piada Ruim do Dia running on port ${port}!`))
